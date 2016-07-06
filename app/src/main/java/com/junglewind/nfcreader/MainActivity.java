c553063ac8889d134015ec0cf87cc3e48941ae70
CX <www.chenxi2008@gmail.com>
2016-07-06 21:23:59 +0800

    check whether device support NFC

--- app/src/main/java/com/junglewind/nfcreader/MainActivity.java
+++ app/src/main/java/com/junglewind/nfcreader/MainActivity.java
@@ -1,13 +1,37 @@
 package com.junglewind.nfcreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_info = (TextView) findViewById(R.id.tv_info);
+
+        if(!checkNfcSupport(this)){
+            tv_info.setText("Sorry , your phone does not support NFC");
+        }
+
+    }
+
+
+    private boolean checkNfcSupport(Context context) {
+        NfcManager manager = (NfcManager) context.getSystemService(Context.NFC_SERVICE);
+        NfcAdapter adapter = manager.getDefaultAdapter();
+        if (adapter != null && adapter.isEnabled()) {
+            // adapter exists and is enabled.
+            return true;
+        }
+
+        return false;
     }
 }
